b8ee4a4bd375d3a26dfbd0050e349e0c1f479cdc
Warat Poovorakit <waratppp@gmail.com>
2024-03-12 05:41:10 +0700

    Initial Commit

---
+++ Movie Searcher/MovieTableViewCell.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var movieYearLabel: UILabel!
+    @IBOutlet var moviePosterImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    static let identifier = "MovieTableViewCell"
+
+    static func nib() -> UINib {
+        return UINib(nibName: "MovieTableViewCell",
+                     bundle: nil)
+    }
+
+    func configure(with model: Movie) {
+        self.movieTitleLabel.text = model.Title
+        self.movieYearLabel.text = model.Year
+        let url = model.Poster
+        if let data = try? Data(contentsOf: URL(string: url)!) {
+            self.moviePosterImageView.image = UIImage(data: data)
+        }
+    }
+    
+}
